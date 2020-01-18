9f1ce99017343afbebd49b7e0ae6e5b312d5c8b8
Donghwi Park <donghwipark@gmail.com>
2020-01-18 12:13:04 +0900

    setting views

--- public/index.php
+++ public/index.php
@@ -1,18 +1,30 @@
 <?php
 
 use DI\Container;
-use Slim\Factory\AppFactory;
-use Slim\Middleware\ErrorMiddleware;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require 'vendor/autoload.php';
 
 $container = new Container();
 AppFactory::setContainer($container);
 
+// Set view in Container
+$container->set('view', function () {
+    return Twig::create('views', [
+        'cache' => false
+    ]);
+});
+
+// Create App
 $app = AppFactory::create();
 
+// Middlewares
+// 1. ErrorMiddleware
 $errorMiddleware = new ErrorMiddleware(
     $app->getCallableResolver(),
     $app->getResponseFactory(),
@@ -23,22 +35,13 @@ $errorMiddleware = new ErrorMiddleware(
 
 $app->add($errorMiddleware);
 
-$container->set('hello', function () {
-    return 'Hello';
-});
+// 2. TwigMiddleware
+$app->add(TwigMiddleware::createFromContainer($app));
 
+// Routing
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write(
-        $this->get('hello')
-    );
-
-    return $response;
+    return $this->get('view')->render($response, 'home.twig');
 });
 
-$app->get('/about', function (Request $request, Response $response) {
-    $response->getBody()->write('About');
-
-    return $response;
-});
-
+// Running App
 $app->run();
